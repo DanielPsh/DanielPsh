94f91455e36cfcad2b40de66048f9fbd6ba2ee09
DP <95006543+DanielPsh@users.noreply.github.com>
2026-06-20 14:51:41 +0800

    .

---
+++ Polyu/COMP2021/Welcome.java
@@ -0,0 +1,8 @@
+package Polyu.COMP2021;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello World 🌎");
+        System.out.println("*".repeat(10));
+    }
+}
