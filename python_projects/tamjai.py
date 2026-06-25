71c066e3a401898dc09eaee823166be0e429ec7b
DP <95006543+DanielPsh@users.noreply.github.com>
2026-06-25 16:14:14 +0800

    edited lists

--- python_projects/tamjai.py
+++ python_projects/tamjai.py
@@ -12,6 +12,15 @@ import math
 # variables
 price = 0
 order_list = []
+# Toppings Vegetables $+4
+TOPPINGS_VEG = [
+    ("", )
+]
+
+# Toppings Meats $+8
+TOPPINGS_MT = [
+    ("", )
+]
 
 # Drinks
 DRINKS = [
@@ -33,8 +42,32 @@ DRINKS = [
     ("Iced Water / Ice", 10),
 ]
 
-Appetizer = [
-    ("TuFei Chicken Wings", 25)
+# Appetizers
+APPETIZER = [
+    ("TuFei Chicken Wings", 25),
+    ("Mala Chicken Wings", 25)
+    ("Chicken Wings with Herb", 25)
+    ("TuFei Chicken Wings & Drumsticks", 25)
+    ("Roasted Pork Intestines", 28)
+    ("Hong Kong Style Curry Fish & Fish Tofu", 12)
+    ("Fresh Vegetables (Lettuce)", 16)
+    ("Fresh Vegetables (Chinese Chives)", 16)
+    ("Fresh Vegetables (Bean Sprout)", 16)
+    ("Fresh Vegetables (Napa Cabbage)", 16)
+    ("Minced Pork", 12)
+]
+
+# Cold Appetizer
+COLD_APPETIZER = [
+    ("Sliced Pork Belly with Mashed Garlic", 30)
+    ("Pig’s Ear in Mala Sauce", 30)
+    ("Homemade Pickles", 12)
+    ("Pink Fungus with Cucumber and Lemon", 25)
+    ("Century Egg in Sichuan Style", 30)
+    ("Black Fungus in Special Chill Sauce", 25)
+    ("Cucumber in Special Chill Sauce", 25)
+    ("Shredded Pork Tripe", 30)
+    ("Pig’s Oviduct in Mala Sauce", 30)
 ]
 
 
@@ -65,6 +98,9 @@ def show_order_list():
     print("==============================\n")
 
 
+# Order drink cold or hot
+
+
 def order_drink(drink_name, base_price):
     # Iced Water / Ice has no temperature option.
     if drink_name == "Iced Water / Ice":
@@ -83,6 +119,8 @@ def order_drink(drink_name, base_price):
 
     print("INVALID Choice. ENTER AGAIN\n")
     return False
+
+# Menu
 
 
 while True:
