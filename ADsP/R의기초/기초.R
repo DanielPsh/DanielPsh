bd27e4013764c733290a3cb2fc819b45428b3cbc
DP <95006543+DanielPsh@users.noreply.github.com>
2026-07-18 13:32:19 +0800

    updated

---
+++ ADsP/R의기초/기초.R
@@ -0,0 +1,26 @@
+class('abc')
+class("abc")
+class('1')
+class('TRUE')
+class(TRUE)
+class(FALSE)
+sqrt(4)
+class(NA)
+class(NULL)
+string1 <- 'abc'
+'data' -> string2
+string1
+string2
+number1 <<- 15
+Inf ->> number2
+logical = NA
+string1 == 'abc'
+string1 != 'abcd'
+string2 > 'DATA'
+number1 <= 15
+is.na(logical)
+is.null(NULL)
+exp(2)
+a1 <- (1:6)
+dim(a1) <- c(2,3)
+a1
