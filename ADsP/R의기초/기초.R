82d9bd54887a504f7c8351e61dd635109930ac91
DP <95006543+DanielPsh@users.noreply.github.com>
2026-07-18 13:40:39 +0800

    updated

--- ADsP/R의기초/기초.R
+++ ADsP/R의기초/기초.R
@@ -24,3 +24,19 @@ exp(2)
 a1 <- (1:6)
 dim(a1) <- c(2,3)
 a1
+m1 <- matrix(c(1:6), nrow = 2)
+colnames(m1) <- c('c1', 'c2', 'c3')
+rownames(m1) <- c('r1', 'r2')
+m1
+colnames(m1)
+rownames(m1)
+df1 <- data.frame(x = c(1,2,3), y = c(4,5,6))
+colnames(df1) <- c('c1', 'c2')
+rownames(df1) <- c('r1','r2','r3')
+df1
+colnames(df1)
+rownames(df1)
+m2 <- matrix(c('180','20','M','150','21','F'), nrow = 2, byrow = T)
+colnames(m2) <- c('height', 'age', 'gender')
+rownames(m2) <- c('Daniel', 'Bobo')
+m2
