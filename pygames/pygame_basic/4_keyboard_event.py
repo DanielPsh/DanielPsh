366e9ec831e473075d0f66d575091b2998cce312
DP <95006543+DanielPsh@users.noreply.github.com>
2026-08-17 14:38:54 +0800

    updated

--- pygames/pygame_basic/4_keyboard_event.py
+++ pygames/pygame_basic/4_keyboard_event.py
@@ -26,21 +26,42 @@ character_x_pos = (screen_width) / 2 - \
     (character_width) / 2  # 화면 가로의 절반 크기에 해당하는 곳에 위치
 character_y_pos = screen_height - character_height  # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
 
-# FPS
-clock = pygame.time.Clock()
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
 # 이벤트 루프
 running = True  # 게임이 진행중인가?
 while running:
-    dt = clock.tick(30)  # 게임화면의 초당 프레임 수를 설정
 
     for event in pygame.event.get():  # 어떤 이벤트가 발생하였는가?
         if event.type == pygame.QUIT:  # 창이 닫히는 이벤트가 발생하였는가?
             running = False  # 게임이 진행중이 아님
 
-    # screen.fill((0, 0, 255))  # RGB 색상 채우기
+        if event.type == pygame.KEYDOWN:  # 키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
+                to_x += 5
+            elif event.key == pygame.K_UP:  # 캐릭터를 위로
+                to_y -= 5
+            elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
+                to_y += 5
+
+        # 키가 떼어졌는지 확인
+        if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+    character_x_pos += to_x
+    character_y_pos += to_y
+
     screen.blit(background, (0, 0))  # 배경 그리기
 
-    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
+    screen.blit(character, (character_x_pos,
+                character_y_pos))  # 캐릭터 그리기
 
     pygame.display.update()  # 게임화면을 다시 그리기! (계속 호출되어야 함)
 
