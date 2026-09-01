b797fedd4c6c7914b9c491a5c4af518c224dde1d
DP <95006543+DanielPsh@users.noreply.github.com>
2026-09-01 15:18:56 +0800

    updated

---
+++ Intro_to_Java/fibonacci.java
@@ -0,0 +1,15 @@
+package Intro_to_Java;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int result = fibonacci(n);
+        System.out.println(result);
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1)
+            return (n);
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    }
+}
